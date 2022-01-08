5a10e65bd236f034b407fabbc4c34e556507c143
abdulazeez2320 <syedabdulazeez2320@gmail.com>
2022-01-08 16:11:02 +0530

    all files are added

---
+++ src/components/Andrabank.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default function Andrabank(props)  {
+   
+        return (
+            <div>
+                <h1>the main branch of  andra bank located {props.location}</h1>
+            </div>
+        );
+    
+        }
+
+export default Andrabank
